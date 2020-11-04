8ec0195be0f06155a9e9630488a72889af2b487a
Kalaiselvan Duraisamy <73700076+KalaiselvanDuraisamy@users.noreply.github.com>
2020-11-04 10:09:06 +0530

    Uploading files

--- Task3/ex1.py
+++
@@ -1,6 +0,0 @@
-from requests_html import HTMLSession
-session = HTMLSession()
-r = session.get('https://www.wego.co.in/')
-#soup = BeautifulSoup(source, 'html.parser')
-text_file = open('abc.txt', 'w')
-text_file.write(r)
